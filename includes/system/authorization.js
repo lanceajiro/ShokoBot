873ce93087093560bbb2aeb1f4cf37a571b45604
lanceajiro <142672718+lanceajiro@users.noreply.github.com>
2024-09-06 14:32:57 +0000

    New

--- includes/system/authorization.js
+++ includes/system/authorization.js
@@ -14,7 +14,6 @@ const request = (bot, chatId, userId) => {
         }
     };
 
-    // Read the contents of manual.txt
     fs.readFile(__dirname + '/manual.txt', 'utf8', (err, manualContent) => {
         if (err) {
             log.erro('Error reading manual.txt: ' + err.message);
@@ -22,7 +21,6 @@ const request = (bot, chatId, userId) => {
             return;
         }
 
-        // Send the manual content first, followed by the policy
         const policyMessage = `
 To use the bot's features, you need to agree to the following policy:
 
@@ -38,10 +36,8 @@ const response = async (bot, callbackQuery) => {
     const userId = callbackQuery.from.id.toString();
     const chatId = callbackQuery.message.chat.id;
     const dataParts = callbackQuery.data.split('_');
-    
-    // Check if this is an authorization action
+
     if (dataParts[0] !== 'auth') {
-        // This is not an authorization action, so we ignore it
         return;
     }
 
@@ -56,12 +52,11 @@ const response = async (bot, callbackQuery) => {
     let responseText;
     if (action === 'agree') {
         const userLog = data.addUser(userId);
-        if (userLog) log.data(userLog); // Log only if the user was newly added
+        if (userLog) log.data(userLog);
         responseText = 'Thank you for agreeing! You have been registered and can now use all bot features.';
     } else if (action === 'disagree') {
         responseText = 'You disagreed with the policy. You can still send messages, but won\'t be able to use bot commands or AI features.';
     } else {
-        // This is an unknown action for the auth module
         await bot.answerCallbackQuery(callbackQuery.id, { text: 'Invalid authorization action.' });
         return;
     }
